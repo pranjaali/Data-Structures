1bd738e5f4e9a7bce7c92017844b87478fc955cf
pranjaali <98611151+pranjaali@users.noreply.github.com>
2022-05-28 20:24:09 +0530

    Update singlylinklist.c

--- singlylinklist.c
+++ singlylinklist.c
@@ -6,7 +6,7 @@ struct node
     struct node *link;
 };
 // ====================(COUNT NODES IN LINKLIST FUNCTION)=========
-int countNodes(struct node **head)
+int countNodes(struct node **head) // Time Complexity: O(N)
 {
     int count = 0;
     struct node *temp = (*head);
@@ -17,9 +17,9 @@ int countNodes(struct node **head)
     else
     {
         count = 1;
-        while (temp->link != NULL)
+        while (temp->link != NULL) // -------------------Time Complexity: O(N)
         {
-            temp=temp->link;
+            temp = temp->link;
             count++;
         }
         return count;
@@ -43,7 +43,7 @@ void insertFirst(struct node **head, int data)
     }
 }
 // -------
-void insertAtLAst(struct node **head, int data)
+void insertAtLAst(struct node **head, int data) // Time Complexity: O(N)
 {
     struct node *temp = (*head);
     struct node *newn = (struct node *)malloc(sizeof(struct node));
@@ -55,7 +55,7 @@ void insertAtLAst(struct node **head, int data)
     }
     else
     {
-        while (temp->link != NULL)
+        while (temp->link != NULL) // -------------------Time Complexity: O(N)
         {
             temp = temp->link;
         }
@@ -64,88 +64,88 @@ void insertAtLAst(struct node **head, int data)
     }
 }
 // ----------------------------
-void insertAtPOsition1(struct node **head, int position, int data)
+void insertAtPOsition1(struct node **head, int position, int data) // Time Complexity: O(N)
 {
     int nPosi = position - 1;
     struct node *temp = (*head);
     struct node *newn = (struct node *)malloc(sizeof(struct node));
-    newn->data = data;
-    newn->link = NULL;
+    newn->data = data; //---------------------O(1)
+    newn->link = NULL; //---------------------O(1)
     if (position == 1)
     {
-        insertFirst(&(*head), data);
+        insertFirst(&(*head), data); //---------------------O(1)
         // newn->link=temp;
         // (*head)=newn;
     }
     else
     {
-        while (nPosi > 1)
+        while (nPosi > 1) // -------------------Time Complexity: O(N)
         {
             temp = temp->link;
             nPosi--;
         }
-        newn->link = temp->link;
-        temp->link = newn;
+        newn->link = temp->link; //---------------------O(1)
+        temp->link = newn;       //---------------------O(1)
     }
 }
 // ----------------------------
 // ====================(DELETION FUNCTIONS)=======================
 void deletFirst(struct node **head)
 {
-    struct node *temp = (*head);
-    (*head) = temp->link;
-    free(temp);
+    struct node *temp = (*head); //---------------------O(1)
+    (*head) = temp->link;        //---------------------O(1)
+    free(temp);                  //---------------------O(1)
 }
-void deleteLast(struct node **head)
+void deleteLast(struct node **head) // Time Complexity: O(N)
 {
-    struct node *temp = (*head), *ptr = temp;
-    while (temp->link != NULL)
+    struct node *temp = (*head), *ptr = temp; //---------------------O(1)
+    while (temp->link != NULL)                // -------------------Time Complexity: O(N)
     {
         ptr = temp;
         temp = temp->link;
     }
-    ptr->link = NULL;
-    free(temp);
+    ptr->link = NULL; //---------------------O(1)
+    free(temp);       //---------------------O(1)
 }
-void deleteAtPosition(struct node **head, int position)
+void deleteAtPosition(struct node **head, int position) // Time Complexity: O(N)
 {
-    int countnodes=countNodes(&(*head));
+    int countnodes = countNodes(&(*head)); //---------------------O(1)
     if (position == 1)
     {
-        deletFirst(&(*head));
+        deletFirst(&(*head)); //---------------------O(1)
     }
-    else if(position>countnodes)
+    else if (position > countnodes)
     {
-        printf("\nLIST LIST HAS %d NODE",countnodes);
-        printf("\n%d th NODE DOSENT EXIST:\n",position);
+        printf("\nLIST LIST HAS %d NODE", countnodes);    //---------------------O(1)
+        printf("\n%d th NODE DOSENT EXIST:\n", position); //---------------------O(1)
     }
     else
     {
-        struct node *temp = (*head), *ptr = temp;
-        int nPos = position;
-        while (nPos > 1)
+        struct node *temp = (*head), *ptr = temp; //---------------------O(1)
+        int nPos = position;                      //---------------------O(1)
+        while (nPos > 1)                          // -------------------Time Complexity: O(N)
         {
             ptr = temp;
             temp = temp->link;
             --nPos;
         }
-        ptr->link = temp->link;
-        free(temp);
+        ptr->link = temp->link; //---------------------O(1)
+        free(temp);             //---------------------O(1)
     }
 }
 
 // ====================(DISPLAY FUNCTION)=========================
-void display(struct node **head)
-{   
+void display(struct node **head) // Time Complexity: O(N)
+{
     if (*head == NULL)
     {
-        printf("the list is empty:\n");
+        printf("the list is empty:\n"); //---------------------O(1)
     }
     else
     {
-        struct node *temp;
-        temp = *head;
-        while (temp != NULL)
+        struct node *temp;   //---------------------O(1)
+        temp = *head;        //---------------------O(1)
+        while (temp != NULL) // ------------------- O(N)
         {
             printf("[%d]-->", temp->data);
             temp = temp->link;
